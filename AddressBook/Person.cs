7947ba5ba27104a017b7579f3dfc1513961ec389
Rutuja412 <rutujakadam4545@gmail.com>
2023-01-12 22:24:17 +0530

    [Rutuja]:Adding Program for create contacts

---
+++ AddressBook/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    public class Person
+    {
+        public static List<Contacts> person = new List<Contacts>();
+        public static void CreateContacts()
+        {
+            Contacts contacts = new Contacts();
+            Console.WriteLine("Enter First Name");
+            contacts.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name");
+            contacts.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter EmailID ");
+            contacts.Email = Console.ReadLine();
+
+            Console.WriteLine("Enter Address");
+            contacts.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter City");
+            contacts.City = Console.ReadLine();
+
+            Console.WriteLine("Enter State");
+            contacts.State = Console.ReadLine();
+
+            Console.WriteLine("Enter Zipcode ");
+            contacts.Zipcode = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Phone Number");
+            contacts.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Created new contact \n");
+
+        }
+}  }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,6 +7,24 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to AddressBook");
-        }
-    }
+            Person person = new Person();
+            while (true)
+            {
+                Console.WriteLine("Welcome to Address Book Program");
+                Console.WriteLine("Select Option:\n" +
+                    "1) Find the Create Contacts\n");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Person.CreateContacts();
+                        break;
+                    default:
+                        Console.WriteLine("Choose valid option");
+                        break;
+                }
+            }
+   }    }
 }
